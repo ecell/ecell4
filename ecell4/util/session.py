e6f7cba6a00891e1768ffdc881820faf4a44eed0
kaizu <kwaizu@gmail.com>
2020-01-21 19:46:44 +0900

    Add a new interface, Session

---
+++ ecell4/util/session.py
@@ -0,0 +1,167 @@
+import copy
+import collections
+import numbers
+import ecell4_base
+
+from . import viz
+from .decorator import get_model
+from .simulation import get_factory, get_shape, number_observer
+
+
+def get_value(obj, type_and_dim=None):
+    return obj
+
+class Result(object):
+
+    def __init__(self, world, observers):
+        self.world = world
+        self.observers = observers
+
+    @property
+    def observer(self):
+        return self.observers[0]
+
+    def plot(self, *args, **kwargs):
+        viz.plot_number_observer(self.observer, *args, **kwargs)
+
+    def data(self):
+        return self.observer.data()
+
+    def species_list(self):
+        return [sp.serial() for sp in self.observer.targets()]
+
+    def as_array(self):
+        import numpy
+        return numpy.array(self.observer.data())
+
+    def as_df(self):
+        return self.as_dataframe()
+
+    def as_dataframe(self):
+        import pandas
+        return pandas.DataFrame(data=self.data(), columns=['t'] + self.species_list())
+
+class Session(object):
+
+    def __init__(self, model=None, y0=None, structures=None, volume=1.0):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        model : Model, optional
+        y0 : dict
+            Initial condition.
+        structures : dict, optional
+            A dictionary which gives pairs of a name and shape of structures.
+            Not fully supported yet.
+        volume : Real or Real3, optional
+            A size of the simulation volume.
+            1.0 as default.
+
+        """
+        self.model = model or get_model()
+        self.y0 = y0.copy() if y0 is not None else {}
+        self.structures = structures.copy() if structures is not None else {}
+        self.volume = copy.copy(volume)
+
+    def run(self, t, solver='ode', rndseed=None, ndiv=None, species_list=None, observers=()):
+        """Run a simulation with the given model and return the result
+
+        Parameters
+        ----------
+        t : array or Real
+            A sequence of time points for which to solve for 'm'.
+        solver : str, tuple or Factory, optional
+            Solver type. Choose one from 'ode', 'gillespie', 'spatiocyte', 'meso',
+            'bd' and 'egfrd'. Default is 'ode'.
+            When tuple is given, the first value must be str as explained above.
+            All the rest is used as arguments for the corresponding factory class.
+            Keyword 's' is a shortcut for specifying 'solver'.
+        rndseed : int, optional
+            A random seed for a simulation.
+        ndiv : int, optional
+            A number of time points. If t is an array, ignored. If None, log all.
+        species_list : list of str, optional
+            A list of names of Species observed. If None, log all.
+            Default is None.
+        observers : Observer or list, optional
+            A list of extra observer references.
+
+        Returns
+        -------
+        ret : Result
+            The result object
+
+        """
+
+        t = get_value(t, ((numbers.Real, '[time]'), ))
+
+        if isinstance(solver, str):
+            f = get_factory(solver)
+        elif isinstance(solver, collections.Iterable):
+            f = get_factory(*solver)
+        else:
+            f = solver  # solver is a Factory
+
+        if rndseed is not None:
+            f = f.rng(ecell4_base.core.GSLRandomNumberGenerator(rndseed))
+
+        volume = get_value(self.volume, ((numbers.Real, '[volume]'), (ecell4_base.core.Real3, '[length]')))
+        w = f.world(volume)
+
+        y0 = {}
+        for key, value in self.y0.items():
+            y0[key] = get_value(value, ((numbers.Real, '[substance]'), ))
+
+        if not isinstance(w, ecell4_base.ode.ODEWorld):
+            w.bind_to(self.model)
+
+        for (name, shape) in self.structures.items():
+            if isinstance(shape, str):
+                w.add_structure(ecell4_base.core.Species(name), get_shape(shape))
+            elif isinstance(shape, collections.Iterable):
+                w.add_structure(ecell4_base.core.Species(name), get_shape(*shape))
+            else:
+                w.add_structure(ecell4_base.core.Species(name), shape)  # shape is a Shape
+
+        if isinstance(w, ecell4_base.ode.ODEWorld):
+            for serial, n in y0.items():
+                w.set_value(ecell4_base.core.Species(serial), n)
+        else:
+            for serial, n in y0.items():
+                w.add_molecules(ecell4_base.core.Species(serial), n)
+
+
+        if isinstance(w, ecell4_base.ode.ODEWorld):
+            ndiv = ndiv or 100
+
+        if isinstance(t, collections.Iterable):
+            upto = t[-1]
+            if species_list is None:
+                obs = ecell4_base.core.TimingNumberObserver(t)
+            else:
+                obs = ecell4_base.core.TimingNumberObserver(t, species_list)
+        elif ndiv is not None:
+            upto = t
+            assert isinstance(ndiv, numbers.Integral) and int(ndiv) > 0
+            t = [float(t) * i / int(ndiv) for i in range(int(ndiv) + 1)]
+            if species_list is None:
+                obs = ecell4_base.core.TimingNumberObserver(t)
+            else:
+                obs = ecell4_base.core.TimingNumberObserver(t, species_list)
+        else:
+            upto = t
+            if species_list is None:
+                obs = ecell4_base.core.NumberObserver()
+            else:
+                obs = ecell4_base.core.NumberObserver(species_list)
+
+        if not isinstance(observers, collections.Iterable):
+            observers = (observers, )
+        observers = (obs, ) + tuple(observers)
+
+        sim = f.simulator(w, self.model)
+        sim.run(upto, observers)
+
+        return Result(w, observers)
