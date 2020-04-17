1fac446aa69a316eebd058033731e271748ab878
kaizu <kwaizu@gmail.com>
2020-04-17 21:03:34 +0900

    Set __all__

--- ecell4/extra/unit.py
+++ ecell4/extra/unit.py
@@ -6,11 +6,13 @@ try:
 except ImportError:
     HAS_PINT = False
     STRICT = False
+    __all__ = ['use_strict']
     # _log.warn("No module named 'pint' required by '{}'".format(__name__))
 else:
     HAS_PINT = True
     STRICT = True
     from ._unit import *
+    __all__ = ['use_strict', 'getUnitRegistry', 'get_application_registry', '_Quantity', '_Unit', 'wrap_quantity']
 
 def use_strict(strict=True):
     global STRICT
