616d37e7c6aab93cb2036b7726ed6a465424f101
Kozo Nishida <kozo.nishida@gmail.com>
2020-06-10 18:14:38 +0900

    Put "cyjs" back again

--- ecell4/util/__init__.py
+++ ecell4/util/__init__.py
@@ -1,5 +1,6 @@
 import collections
 
+from .cyjs import plot_species
 from .decorator import reaction_rules, species_attributes, get_model, reset_model
 from . import viz
 from .simulation import run_simulation, ensemble_simulations, number_observer
@@ -8,6 +9,6 @@ from .show import show
 from .session import Session, load_world
 
 __all__ = [
-    'run_simulation', 'ensemble_simulations', 'load_world', 'number_observer',
+    'plot_species', 'run_simulation', 'ensemble_simulations', 'load_world', 'number_observer',
     'reaction_rules', 'species_attributes', 'get_model', 'show',
     'viz', 'ports', 'Session']
