f38da950d2bcbea12e93d47346604b1d1396aec2
Kozo Nishida <kozo.nishida@gmail.com>
2021-01-18 06:23:39 +0900

    Update version
    The problematic 1.2.0 couldn't be uploaded because it was already uploaded to PyPI.
    See https://pypi.org/help/#file-name-reuse

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESCRIPTION = open(os.path.join(".", "README.md")).read()
 
 setup(
     name='ecell4',
-    version='1.2.0',
+    version='1.2.1',
     packages=['ecell4', 'ecell4.util', 'ecell4.extra', 'ecell4.datasource', 'ecell4.mca', 'ecell4.plotting'],
     package_data = {"ecell4.util": [
         "templates/init_ipynb.js", "templates/init_cyjs.js", "templates/template.html",
